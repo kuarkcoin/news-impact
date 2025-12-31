a2c95028fb15cc6feabd53034feb8fc5f3430efe
kuarkcoin <30313951+kuarkcoin@users.noreply.github.com>
2025-12-31 18:02:18 +0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
@@ -10,3 +9,4 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
